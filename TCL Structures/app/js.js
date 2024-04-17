c3fbc3fc1bc62c084e6a801ff97503e85fde1867
MikeZeg <michalzegzula@gmail.com>
2024-04-17 20:09:34 +0100

    add JS file plus imgs

---
+++ TCL Structures/app/js.js
@@ -0,0 +1,13 @@
+// Swap main__home background
+
+
+// Change images About
+const grabSwap = document.querySelector('#about > div > div.main__about__imgs__swap')
+
+
+console.log(grabSwap , 'asd')
+
+//Our Service change boxes
+
+//Header stay stick in top position or arrow on side
+
