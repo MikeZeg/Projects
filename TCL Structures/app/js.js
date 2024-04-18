638b5735844dd802c3596b53e461ffc1b37ed1c7
MikeZeg <michalzegzula@gmail.com>
2024-04-18 20:36:50 +0100

    add JS imgs changes option

--- TCL Structures/app/js.js
+++ TCL Structures/app/js.js
@@ -1,11 +1,17 @@
 // Swap main__home background
+const homeImgSwap = document.querySelector('.main__home')
+console.log(homeImgSwap.style.backgroundImage.style)
+
+
+console.log('change img');
+homeImgSwap.style.backgroundImage = "url('../../../Images/background/background3.jpg')";
+
+// homeImgSwap.style
 
 
 // Change images About
 const grabSwap = document.querySelector('#about > div > div.main__about__imgs__swap')
 
-
-console.log(grabSwap , 'asd')
 
 //Our Service change boxes
 
