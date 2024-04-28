c24818149d4102d8a883e71c8e6aa376bd3ac502
MikeZeg <michalzegzula@gmail.com>
2024-04-28 19:32:05 +0100

    add switch imgs in about section

--- TCL Structures/app/js.js
+++ TCL Structures/app/js.js
@@ -1,4 +1,4 @@
-// Swap main__home background
+// -------> Swap main__home background <---------
 let flag = false;
 
 const changeBackgroundImage = () => {
@@ -12,17 +12,26 @@ const changeBackgroundImage = () => {
         homeImg.style.backgroundImage = "url('./Images/background/background1.jpg')"
         flag = false;
     }
+};
+setInterval(changeBackgroundImage,5000);
 
-    
-}
-setInterval(changeBackgroundImage,5000)
+// --------> Change images About <----------
+const grabSwap = document.querySelector('.main__about__imgs__swap');
+const leftArrow = document.querySelector('.main__about__imgs__left');
+const rightArrow = document.querySelector('.main__about__imgs__right');
+
+let number = 1;
+
+leftArrow.addEventListener('click',()=>{
+    console.log('Click left')
+})
+rightArrow.addEventListener('click',()=>{
+    console.log('Click right')
+})
 
 
-
-// Change images About
-const grabSwap = document.querySelector('#about > div > div.main__about__imgs__swap')
-// console.log(grabSwap)
-
+// ${number} add to the end of URL
+// grabSwap.style.backgroundImage = `url(./Images/about/about${number}.jpg)`
 //Our Service change boxes
 
 //Header stay stick in top position or arrow on side
